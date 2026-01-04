5670c84b0fe6ac5a4b7bc83433fa79df5906dcfe
42Wor <wwork4287@gmail.com>
2026-01-04 16:25:49 +0500

    1.7.0

--- aceflow/__init__.py
+++ aceflow/__init__.py
@@ -1,6 +1,6 @@
 from .core.model import Seq2SeqModel
 from .utils.tokenizer import Tokenizer
-from .trainers import Trainer
+from .trainers import Trainer  # Only import Trainer here
 from importlib.metadata import version as _version
 
 version = _version("aceflow") 
